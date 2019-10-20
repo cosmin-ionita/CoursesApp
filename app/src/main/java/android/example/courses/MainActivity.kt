a4026f2061409f17acf47b165e4af714bd871cd1
Cosmin Ionita <ionita.cosmin@outlook.com>
2019-10-20 19:39:14 +0300

    [UI] - Add Navigation Activity template

--- app/src/main/java/android/example/courses/MainActivity.kt
+++ app/src/main/java/android/example/courses/MainActivity.kt
@@ -1,12 +1,19 @@
 package android.example.courses
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginClick(view: View) {
+        val intent = Intent(this, NavigationActivity::class.java)
+        startActivity(intent)
     }
 }
